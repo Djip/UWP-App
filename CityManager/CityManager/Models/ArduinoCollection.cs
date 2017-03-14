09d9c5220bfaf36e58ecf3ac3b1c8dde2b866f79
Delta135 <135.delta@gmail.com>
2017-03-14 13:16:17 +0100

    it's a start...

--- CityManager/CityManager/Models/ArduinoCollection.cs
+++ CityManager/CityManager/Models/ArduinoCollection.cs
@@ -8,10 +8,27 @@ using System.Xml.Serialization;
 namespace CityManager.Models
 {
     [XmlRoot("ArduinoCollection")]
+
     public class ArduinoCollection
     {
         [XmlArray("Arduinos")]
         [XmlArrayItem("Arduino", typeof(Arduino))]
         public Arduino[] Arduinos { get; set; }
+
+        public static ArduinoCollection Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new ArduinoCollection();
+                }
+                return instance;
+            }
+        }
+
+        private static ArduinoCollection instance;
+        private ArduinoCollection() { }
+        
     }
 }
